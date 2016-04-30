a1a9b5aa997dbf8f0634ee96d984dfdb8fa93444
Lindsey Baker <lindseybakermedia@gmail.com>
2016-04-30 00:22:12 -0700

    deletes map code

--- Neighborgoods/ViewController.swift
+++ Neighborgoods/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreLocation
 import MapKit
 
 let businessName = "Bread Basket Bakery"
@@ -17,37 +16,21 @@ let businessPhone = "(480) 423-0113"
 let businessUrl = "http://breadbasketbakeryscottsdale.com/"
 var location = CLLocationCoordinate2D()
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet var map: MKMapView!
     
-    var locationManager = CLLocationManager()
     var myPosition = CLLocationCoordinate2D()
     var mapItem = MKMapItem()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        locationManager.delegate = self
-//        locationManager.requestWhenInUseAuthorization()
-//        locationManager.startUpdatingLocation()
         
         CLGeocoder().geocodeAddressString("\(businessAddress1), \(businessAddress2)", completionHandler: { (placemarks, error) -> Void in
             if (error == nil) {
                 if let p = placemarks?[0] {
                     location = p.location!.coordinate
-                    let businessCoordinate = CLLocationCoordinate2DMake(location.latitude, location.longitude)
-                    let annotation = MKPointAnnotation()
-                    annotation.coordinate = businessCoordinate
-                    annotation.title = businessName
-                    annotation.subtitle = businessAddress1
-                    self.map.addAnnotation(annotation)
-                    
-                    let span = MKCoordinateSpanMake(0.05, 0.05)
-                    let region = MKCoordinateRegionMake(location, span)
-                    self.map.setRegion(region, animated: true)
-                    
                     self.mapItem = MKMapItem(placemark: MKPlacemark(placemark: p))
-
                 }
             }
         })
@@ -61,23 +44,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
-        myPosition = newLocation.coordinate
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegionMake(newLocation.coordinate, span)
-        map.setRegion(region, animated: true)
-        // locationManager.stopUpdatingLocation()
-    }
-    
     @IBAction func directions(sender: AnyObject) {
-        print("\(location.latitude), \(location.longitude)")
         mapItem.name = businessName
         mapItem.phoneNumber = businessPhone
         mapItem.url = NSURL(string: businessUrl)
         mapItem.openInMapsWithLaunchOptions(nil)
-        
     }
-
-
 }
 
